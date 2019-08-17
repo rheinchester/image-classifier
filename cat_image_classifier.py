41506dce49e15598328ab6666a14700047c662fa
Jacob okoro <jaycobokoro@gmail.com>
2019-08-17 18:13:41 +0100

    update cat_image_classifier.py
    Signed-off-by: Jacob okoro <jaycobokoro@gmail.com>

--- cat_image_classifier.py
+++ cat_image_classifier.py
@@ -1,11 +1,12 @@
 # %%
-""" My first cat image classifier
+""" 
+    Author: Jacob Okoro (jaycobokoro@gmail.com)
     From Andrew ng's neural network deep learning specialization
 """
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py
-import scipy;
+import scipy
 from PIL import Image
 from scipy import ndimage
 import random # for testing purposes
@@ -14,10 +15,8 @@ def print_image(index):
     index = 25
     train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
     plt.imshow(train_set_x_orig[5])
-    print("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") + "' picture.")
-
-
-
+    print("y = " + str(train_set_y[:, index]) + ", it's a '" + \
+        classes[np.squeeze(train_set_y[:, index])].decode("utf-8") + "' picture.")
 
 def sigmoid(z):
     """ convert a number to sigmoid
@@ -91,8 +90,8 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost=False):
         # retrieve parameters
         dw, db = grads['dw'], grads['db']
         # update parameters
-        w = w - (learning_rate*dw)
-        b = b - (learning_rate*db)
+        w = w - (learning_rate * dw)
+        b = b - (learning_rate * db)
         # record cost for every 100th iteration
         if i % 100 == 0:
             costs.append(cost)
@@ -123,8 +122,7 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0
     # initialize parameters with zeros
     w, b = zero_param_init(X_train.shape[0])
     # Run Gradient descent 
-    grads, params, costs = optimize(
-        w, b, X_train, Y_train, num_iterations, learning_rate)
+    grads, params, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate)
     # Retrieve parameters w and b, after descent
     w, b = params['w'], params['b']
     # Predict test/train set examples
@@ -160,8 +158,8 @@ def load_dataset():
     test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
     return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
-def test_function(my_image, num_px):
-    # We preprocess the image to fit your algorithm.
+def classify_image(my_image, num_px):
+    # We preprocess the image to fit the algorithm.
     fname = "images/" + my_image
     image = np.array(ndimage.imread(fname, flatten=False))
     image = image/255.
@@ -169,9 +167,9 @@ def test_function(my_image, num_px):
     my_predicted_image = predict(d["w"], d["b"], my_image)
     plt.imshow(image)
     print("y = " + str(np.squeeze(my_predicted_image)) + ", your algorithm predicts a \"" + classes[int(np.squeeze(my_predicted_image)), ].decode("utf-8") + "\" picture.")
-# END CODE HERE ##
 
 
+np.random.rand()
 #%%
 
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
@@ -180,15 +178,16 @@ standard_test_x = flatten_and_standardize(test_set_x_orig)
 
 d = model(standard_train_x, train_set_y, standard_test_x, test_set_y, num_iterations=2000, learning_rate=0.005, print_cost=True)
 
-costs = np.squeeze(d['costs'])
-plt.plot(costs)
-plt.ylabel('cost')
-plt.xlabel('iterations (per hundreds)')
-plt.title("Learning rate =" + str(d["learning_rate"]))
-plt.show()
+# costs = np.squeeze(d['costs'])
+# plt.plot(costs)
+# plt.ylabel('cost')
+# plt.xlabel('iterations (per hundreds)')
+# plt.title("Learning rate =" + str(d["learning_rate"]))
+# plt.show()
 
-
-my_image = "dog_image1.jpg"
+#######INSERT IMAGE HERE########
+my_image = "dog_image2.jpg"
 num_px= 64
-test_function(my_image, num_px)
-#%%
+classify_image(my_image, num_px)
+
+
